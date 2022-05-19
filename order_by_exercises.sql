4ae6046b00e65ce0a6b36fd2c508be8e9b9c6098
tim-keriazes <tim.keriazes@gmail.com>
2022-05-19 10:15:53 -0700

    order by exercises added

---
+++ order_by_exercises.sql
@@ -0,0 +1,28 @@
+use employees;
+select * from employees
+where first_name in('Irena', 'Vidya', 'Maya') order by first_name;
+-- returned Irena Reutenauer for first and Vidya Simmen
+
+select * from employees
+where first_name in('Irena', 'Vidya', 'Maya') order by first_name, last_name;
+-- returned Irena Acton for first and Vidya Zweizig
+
+select * from employees
+where first_name in('Irena', 'Vidya', 'Maya') order by last_name, first_name;
+-- returned Irena Acton for first and Maya Zyda
+
+select * from employees
+where last_name like ('e%') and last_name like ('%e')
+order by emp_no;
+-- returned 899 rows, the first being Ramzi Erde and last being Tadahiro Erde
+
+select * from employees
+where last_name like ('e%') and last_name like ('%e')
+order by hire_date desc;
+-- returned 899 rows, the newest being Teiji Eldridge and oldest being Fox Erie
+
+select * from employees
+where hire_date like '1990%'
+and birth_date like '%-12-25'
+order by hire_date desc , birth_date desc;
+-- returned 77 rows Alair Pezzoli being the oldest hired last and Alselm Cappello being the youngest hired earliest
